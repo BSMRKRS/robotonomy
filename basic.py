8a12d8d7a5d42db734bcca06c9c405af6b34b8c5
Jack <jwrickman18@bsmschool.org>
2017-10-09 12:40:21 -0500

    Added back, left, and right distance sensors.

--- basic.py
+++ basic.py
@@ -1,9 +1,16 @@
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
+#sensors
+back_sensor = 17
+starboard_sensor = 18
+port_sensor = 19
 
-sensor_pin = 17
-RPL.pinMode(sensor_pin,RPL.INPUT)
+RPL.pinMode(17,RPL.INPUT)
+RPL.pinMode(18,RPL.INPUT)
+RPL.pinMode(19,RPL.INPUT)
 
 while True:
-  PTW.state['d1'] = RPL.digitalRead(sensor_pin)
+  PTW.state['d1'] = RPL.digitalRead(back_sensor)
+  PTW.state['d2'] = RPL.digitalRead(starboard_sensor)
+  PTW.state['d3'] = RPL.digitalRead(port_sensor)
   PTW.post()
