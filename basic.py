77be9dbe700a631b838133b5ebf6cfc2ea76ebea
Jack <jwrickman18@bsmschool.org>
2017-10-10 12:39:51 -0500

    fixed merge conflicts

--- basic.py
+++ basic.py
@@ -14,14 +14,7 @@ tState = time.time()
 while True:
     global tState
     if time.time() - tState > 0.5:
-<<<<<<< HEAD
-        print RPL.digitalRead(back_sensor)
-        print RPL.digitalRead(starboard_sensor)
-        print RPL.digitalRead(port_sensor)
-        tState = time.time()
-=======
         print RPL.readDistance(back_sensor)
         print RPL.readDistance(starboard_sensor)
         print RPL.readDistance(port_sensor)
-            tState = time.time()
->>>>>>> 0a55936c447eb1eb0508a4dbb9eb7ae038beb99b
+        tState = time.time()
