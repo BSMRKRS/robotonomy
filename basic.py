3e64863641c762fbce8bf29b23da56ca0ea73768
Jack <jwrickman18@bsmschool.org>
2017-10-13 12:41:31 -0500

    Switched basic.py back to ptw settings

--- basic.py
+++ basic.py
@@ -1,7 +1,7 @@
 from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
-#sensors
 import time as time
+#sensors
 back_sensor = 17
 starboard_sensor = 18
 port_sensor = 19
@@ -11,10 +11,9 @@ RPL.pinMode(18,RPL.INPUT)
 RPL.pinMode(19,RPL.INPUT)
 
 tState = time.time()
+
 while True:
-    global tState
-    if time.time() - tState > 0.5:
-        print RPL.readDistance(back_sensor)
-        print RPL.readDistance(starboard_sensor)
-        print RPL.readDistance(port_sensor)
-        tState = time.time()
+  PTW.state['d1'] = RPL.digitalRead(starboard_sensor)
+  PTW.state['d2'] = RPL.digitalRead(port_sensor)
+  PTW.state['d3'] = RPL. digitalRead(back_sensor)
+  PTW.post()
