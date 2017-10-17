e7faba7912228ebfa9ebdca49829f7bd52c9b8d3
Jack <jwrickman18@bsmschool.org>
2017-10-17 12:26:18 -0500

    I had two copies of userInterface function. Derp

--- basic.py
+++ basic.py
@@ -26,13 +26,5 @@ def post(interval = 0.5):
     userInterface()
     tState = time.time()
 
-def userInterface():
-    rightSensor = RPL.digitalRead(starboard_sensor)
-    leftSensor = RPL.digitalRead(port_sensor)
-    backSensor = RPL.digitalRead(back_sensor)
-    print "Right: %d" %rightSensor
-    print "Left: %d"  %leftSensor
-    print "Back: %d"  %backSensor
-
 while True:
     post()
