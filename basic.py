bcac0631662d448dc64733612182e38dd5d9e2d6
Jack <jwrickman18@bsmschool.org>
2017-10-17 12:14:01 -0500

    Changed digital reads to readDistance to get distance in mil

--- basic.py
+++ basic.py
@@ -28,9 +28,9 @@ def post(interval = 0.5):
 
 def userInterface():
   clear()
-  rightSensor = RPL.digitalRead(starboard_sensor)
-  leftSensor = RPL.digitalRead(port_sensor)
-  backSensor = RPL. digitalRead(back_sensor)
+  rightSensor = RPL.readDistance(starboard_sensor)
+  leftSensor = RPL.readDistance(port_sensor)
+  backSensor = RPL. readDistance(back_sensor)
   print "Right: %d" %rightSensor
   print "Left: %d"  %leftSensor
   print "Back: %d"  %backSensor
