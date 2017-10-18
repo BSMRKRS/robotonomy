31b8f198d4f7358825dbe7fd2ff74d79ce08c9dd
Jack <jwrickman18@bsmschool.org>
2017-10-18 12:18:24 -0500

    analogue readback on pin 16

--- basic.py
+++ basic.py
@@ -2,19 +2,23 @@ from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
 import time as time
 #sensors
+analogue = 16
 back_sensor = 17
 starboard_sensor = 18
 port_sensor = 19
 
+RPL.pinMode(16,RPL.INPUT)
 RPL.pinMode(17,RPL.INPUT)
 RPL.pinMode(18,RPL.INPUT)
 RPL.pinMode(19,RPL.INPUT)
 
 def userInterface():
   print("\033c")
+  analogue = RPL.readDistance(analogue)
   rightSensor = RPL.digitalRead(starboard_sensor)
   leftSensor = RPL.digitalRead(port_sensor)
   backSensor = RPL.digitalRead(back_sensor)
+  print "LeftAn: %d" %analogue
   print "Right: %d" %rightSensor
   print "Left: %d"  %leftSensor
   print "Back: %d"  %backSensor
