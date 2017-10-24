264d5d5431bef1cf00f406ecca6678cb443af1e8
Jack <jwrickman18@bsmschool.org>
2017-10-24 12:39:41 -0500

    Added front and back digital read sensors

--- basic.py
+++ basic.py
@@ -3,9 +3,10 @@ import post_to_web as PTW # see post_to_web.py for instructions
 import time as time
 #sensors
 analogue_pin = 16
-back_sensor = 17
-starboard_sensor = 18
-port_sensor = 19
+back_sensor_pin = 17
+starboard_sensor_pin = 18
+port_sensor_pin = 19
+front_sensor_pin = 23
 
 RPL.pinMode(16,RPL.INPUT)
 RPL.pinMode(17,RPL.INPUT)
@@ -14,14 +15,12 @@ RPL.pinMode(19,RPL.INPUT)
 
 def userInterface():
   print("\033c")
-  rightSensor = RPL.digitalRead(starboard_sensor)
-  leftSensor = RPL.digitalRead(port_sensor)
-  backSensor = RPL.digitalRead(back_sensor)
-  print "Right: %d" %rightSensor
-  print "Left: %d"  %leftSensor
-  print "Back: %d"  %backSensor
-  analogue = RPL.readDistance(analogue_pin)
-  print "LeftAn: %d" %analogue
+  #rightSensor = RPL.digitalRead(starboard_sensor)
+  #leftSensor = RPL.digitalRead(port_sensor)
+  backSensorRead = RPL.digitalRead(back_sensor)
+  frontSensorRead = RPL.digitalRead(front_sensor_pin)
+  print "Front: %d"  %frontSensorReadSensor
+  print "Back: %d"  %backSensorRead
 
 
 tState = time.time()
