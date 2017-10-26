15edbc3afffd21a3e4f482773d6313b7e960a35b
Jack <jwrickman18@bsmschool.org>
2017-10-26 07:47:17 -0500

    Added comments

--- basic.py
+++ basic.py
@@ -2,15 +2,22 @@ from setup import RPL
 import post_to_web as PTW # see post_to_web.py for instructions
 import time as time
 import control as con
-#sensors
+#####################
+#########PINS########
+#####################
+
+#sensor pins
 analogue_pin = 16
 back_sensor_pin = 17
 starboard_sensor_pin = 18
 port_sensor_pin = 19
 front_sensor_pin = 23
+
+#servo pins
 left_servo_pin = 0
 right_servo_pin = 1
 
+#pin setup
 RPL.pinMode(left_servo_pin,RPL.OUTPUT)
 RPl.pinMode(right_servo_pin,RPL.OUTPUT)
 RPL.pinMode(16,RPL.INPUT)
@@ -18,7 +25,12 @@ RPL.pinMode(17,RPL.INPUT)
 RPL.pinMode(18,RPL.INPUT)
 RPL.pinMode(19,RPL.INPUT)
 
-def userInterface():
+
+##########################
+#####control functions####
+##########################
+
+def userInterface(): #reads the digital sensor inputs and contains movement autonomy
   print("\033c")
   #rightSensor = RPL.digitalRead(starboard_sensor)
   #leftSensor = RPL.digitalRead(port_sensor)
@@ -35,11 +47,11 @@ def userInterface():
 
 tState = time.time()
 
-def post(interval = 0.25):
+def post(interval = 0.25): #controls the time intervals that the sensors and ai refresh at
   global tState
   if time.time() - tState > interval:
     userInterface()
     tState = time.time()
 
-while True:
+while True: #run function
     post()
