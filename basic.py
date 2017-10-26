444852c5c67faeb922cdfacb587928bf78ade415
Jack <jwrickman18@bsmschool.org>
2017-10-26 14:26:19 -0500

    Added logic for all 16 senerios possible w/ 4 binary sensors.

--- basic.py
+++ basic.py
@@ -33,35 +33,77 @@ def stopAll():
     RPL.servoWrite(right_servo_pin,1500)
 
 def userInterface(): #reads the digital sensor inputs and contains movement autonomy
-  print("\033c")
-  starboardSensorRead = RPL.digitalRead(starboard_sensor_pin)
-  portSensorRead = RPL.digitalRead(port_sensor_pin)
-  backSensorRead = RPL.digitalRead(back_sensor_pin)
-  frontSensorRead = RPL.digitalRead(front_sensor_pin)
-  print "Front: %d" %frontSensorRead
-  print "Back: %d"  %backSensorRead
-  print "Left: %d"  %portSensorRead
-  print "Right: %d" %starboardSensorRead
-  if starboardSensorRead == 1:
-      con.right()
-      time.sleep(0.5)
-      con.forward()
-      time.sleep(0.5)
-      con.left()
-      time.sleep(0.5)
-      print "searching for wall"
-  elif starboardSensorRead == 0 and frontSensorRead == 1:
-      con.forward
-      print "forward"
-  elif starboardSensorRead == 0 and frontSensorRead == 0 and portSensorRead == 1:
-      con.left()
-      print "avoiding wall ahead and right"
-  elif frontSensorRead == 0 and backSensorRead == 1 and starboardSensorRead == 0 and portSensorRead == 0:
-      con.reverse()
-      print "reverse"
-  else:
-      stopAll()
-      print "stop"
+    print("\033c")
+    sensors = []
+    sensors.append(RPL.digitalRead(front_sensor_pin))
+    sensors.append(RPL.digitalRead(starboard_sensor_pin))
+    sensors.append(RPL.digitalRead(back_sensor_pin))
+    sensors.append(RPL.digitalRead(port_sensor_pin))
+
+    print "Front: %d" %frontSensorRead
+    print "Right: %d" %starboardSensorRead
+    print "Back: %d"  %backSensorRead
+    print "Left: %d"  %portSensorRead
+
+#######################
+######## Logic ########
+#######################
+    if sensors = [0,0,0,0]: #Completely Surrounded
+        print "Trapped!"
+    elif sensors = [0,0,0,1]: #Walls Front, Right, Behind
+        print "Attempting to escape left"
+        con.left()
+    elif sensors = [0,0,1,0]: #Walls Front, Right, Left
+        print "Dead End. Reversing."
+        con.reverse()
+        time.sleep(3)
+    elif sensors = [0,0,1,1]: #Walls Front, Right. Front Right Corner
+        print " Front Right Corner. Turning Left"
+        con.left()
+    elif sensors = [0,1,0,0]: #Walls Front, Behind, Left. Parral Parked Left
+        print "Attempting to escape Right."
+        con.right()
+        time.sleep(0.25)
+        con.reverse()
+    elif sensors = [0,1,0,1]: #Walls Front, Behind.
+        print "Blocked front and back. Escaping Right"
+        con.right()
+        time.sleep(0.25)
+        con.reverse()
+    elif sensors = [0,1,1,0]: #Walls Front, Left.
+        print "Blocked Front and Left."
+        con.right()
+    elif sensors = [0,1,1,1]: #Wall Front.
+        print "Wall ahead. Turning left."
+        con.reverse()
+        time.sleep(0.25)
+        con.left()
+    elif sensors = [1,0,0,0]: #Walls Behind, Left, Right
+        print "Backed in. Escaping forward"
+        con.forward
+    elif sensors = [1,0,0,1]: #Walls Right, Behind. Back Right Corner.
+        print "Right Wall found. Proceeding Forward."
+        con.forward
+    elif sensors = [1,0,1,0]: #Walls Right, Left. Cooridor.
+        print "In a cooridor. Proceeding Forward."
+        con.forward()
+    elif sensors = [1,0,1,1]: #Wall Right.
+        print "Right Wall found. Proceeding Forward."
+        con.forward()
+    elif sensors = [1,1,0,0]: #Wall Behind, Left. Back Left Corner
+        print "Wall Behind and Left. Turning Right."
+        con.right()
+    elif sensors = [1,1,0,1]: #Wall Behind.
+        print "Wall Behind. Turning Right."
+        con.right()
+    elif sensors = [1,1,1,0]: #Wall Left
+        print "Wall Left. Turning around."
+        con.right()
+        time.sleep(2)
+    elif sensors = [1,1,1,1]:
+        print "No Wall Aquired. Finding Wall."
+        con.forward()
+
 
 tState = time.time()
 
